b8dda8050635df927f115c3fd460da05b173a284
fentosv <fentos.dev@gmail.com>
2023-07-18 19:11:25 +0200

    Add service_worker to block urls

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,11 +1,52 @@
-import reloadOnUpdate from "virtual:reload-on-update-in-background-script";
+const REDIRECT = chrome.declarativeNetRequest.RuleActionType.REDIRECT
+const MAIN_FRAME = chrome.declarativeNetRequest.ResourceType.MAIN_FRAME
+const SUB_FRAME = chrome.declarativeNetRequest.ResourceType.SUB_FRAME
+const XMLHTTPREQUEST = chrome.declarativeNetRequest.ResourceType.XMLHTTPREQUEST
 
-reloadOnUpdate("pages/background");
+const redirectExtensionPath = '/src/blocked/blocked.html'
 
-/**
- * Extension reloading is necessary because the browser automatically caches the css.
- * If you do not use the css of the content script, please delete it.
- */
-reloadOnUpdate("pages/content/style.scss");
+console.log('Service worker started.')
 
-console.log("background loaded");
+const blockUrls = ['||example', '||example1', '||examples']
+
+chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((e) => {
+  const msg = `Navigation blocked to ${e.request.url} on tab ${e.request.tabId}.`
+  console.log(msg)
+})
+
+blockUrls.forEach((domain, index) => {
+  let id = index + 1
+
+  chrome.declarativeNetRequest.updateDynamicRules({
+    addRules: [
+      {
+        id: id,
+        priority: 1,
+        action: {
+          type: REDIRECT,
+          redirect: {
+            extensionPath: redirectExtensionPath,
+          },
+        },
+        condition: {
+          urlFilter: domain,
+          resourceTypes: [MAIN_FRAME, SUB_FRAME, XMLHTTPREQUEST],
+        },
+      },
+    ],
+    removeRuleIds: [id],
+  })
+})
+
+// chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+//   const keywords = ['example']
+//   console.log(changeInfo.title, changeInfo.url)
+//   if (keywords && changeInfo.title) {
+//     const { title } = changeInfo
+//     const containsKeyword = keywords.some((keyword) => title.toLowerCase().includes(keyword))
+//     if (containsKeyword) {
+//       console.log('Lo contiene')
+//       chrome.tabs.update(tabId, { url: //todo redirectExtensionPath })
+//     }
+//   }
+// })
