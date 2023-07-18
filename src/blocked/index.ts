92342b69f3c01ddb2ab22c81cca6cf53a327898e
fentosv <fentos.dev@gmail.com>
2023-07-18 19:04:40 +0200

    Add blocked custom page

--- src/blocked/index.ts
+++ src/blocked/index.ts
@@ -1,12 +1,12 @@
 import '../app.css'
-import Counter from '../components/Counter.svelte'
+import Blocked from '../components/Blocked.svelte'
 
 const target = document.getElementById('app')
 
 async function render() {
   const { count } = await chrome.storage.sync.get({ count: 0 })
 
-  new Counter({ target, props: { count } })
+  new Blocked({ target, props: { count } })
 }
 
 document.addEventListener('DOMContentLoaded', render)
---
+++ src/components/Blocked.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let count: number
+  let message: string = null
+</script>
+
+<section>
+  <h1>Don't give up, you're doing great</h1>
+  <p>
+    Current time blocking garbage: <span>{count}</span>
+  </p>
+</section>
+
+<style scoped>
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+  h1 {
+    font-size: 2rem;
+    color: crimson;
+  }
+  span {
+    font-weight: 600;
+  }
+</style>
