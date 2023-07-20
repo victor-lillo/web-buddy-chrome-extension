c484ccc29cfbbd5cf77525d0948c8167ae1716a9
fentosv <fentos.dev@gmail.com>
2023-07-20 21:00:07 +0200

    Add chrome.storage methods

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,13 +1,33 @@
 type IStorage = {
-    count: number;
-};
+  count: number
+  installDate?: Date
+}
 
 const defaultStorage: IStorage = {
-    count: 0,
-};
+  count: 0,
+}
 
 export const storage = {
-    get: (): Promise<IStorage> =>
-        chrome.storage.sync.get(defaultStorage) as Promise<IStorage>,
-    set: (value: IStorage): Promise<void> => chrome.storage.sync.set(value),
-};
+  get: (): Promise<IStorage> => chrome.storage.sync.get(defaultStorage) as Promise<IStorage>,
+  set: (value): Promise<void> => chrome.storage.sync.set(value),
+}
+
+export async function setStorage(value: { [key: string]: string | number }) {
+  await chrome.storage.sync.set(value)
+}
+
+export async function getStorage(key: string) {
+  const result = await getFullStorage()
+  console.log(`"${key}" value is: ${result[key]}`)
+  return result[key]
+}
+
+export async function getFullStorage() {
+  const result = await chrome.storage.sync.get()
+  console.log('Full Storage is: ' + JSON.stringify(result))
+  return result
+}
+
+export async function clearStorage() {
+  await chrome.storage.sync.clear()
+}
