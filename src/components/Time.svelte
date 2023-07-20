2b2fd586a9d4dedd7f860ed878cf9fab043bbcbc
fentosv <fentos.dev@gmail.com>
2023-07-20 21:00:21 +0200

    Add Time component

---
+++ src/components/Time.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let installDate: number
+  let message = null
+
+  const rtf1 = new Intl.RelativeTimeFormat('en', { style: 'short' })
+</script>
+
+<section>
+  <h1>
+    Current time blocking sites:&nbsp;<strong>{installDate}</strong>
+  </h1>
+</section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    white-space: nowrap;
+  }
+
+  h1 {
+    display: flex;
+  }
+</style>
