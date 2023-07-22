7dcb1dbe770c20b1f5590c9fe34f626ae6f04cb7
fentosv <fentos.dev@gmail.com>
2023-07-22 12:43:21 +0200

    Comment inserting css on page

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,11 +1,10 @@
-import Counter from '../components/Counter.svelte'
 import { storage } from '../utils/storage'
 
 // Some global styles on the page
-import './styles.css'
+// import './styles.css'
 
 // Some JS on the page
 storage.get().then(console.log)
 
 // Some svelte component on the page
-new Counter({ target: document.body })
+// new Counter({ target: document.body })
