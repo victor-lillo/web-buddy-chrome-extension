89b0699c448f8721dd49385495af773ecd2c95e7
fentosv <fentos.dev@gmail.com>
2023-07-26 17:34:21 +0200

    Add time calculations

--- src/utils/relativeDate.ts
+++ src/utils/relativeDate.ts
@@ -1,9 +1,14 @@
-const DAY_MILLISECONDS = 1000 * 60 * 60 * 24
+const MIN_MILLISECONDS = 1000 * 60
 const HOUR_MILLISECONDS = 1000 * 60 * 60
+const DAY_MILLISECONDS = 1000 * 60 * 60 * 24
+const MONTH_MILLISECONDS = 1000 * 60 * 60 * 24 * 30
 
 enum FORMATS {
-  day = 'day',
+  second = 'second',
+  minute = 'minute',
   hour = 'hour',
+  day = 'day',
+  month = 'month',
 }
 
 const rtf = new Intl.RelativeTimeFormat('en', {
@@ -11,8 +16,30 @@ const rtf = new Intl.RelativeTimeFormat('en', {
   style: 'long',
 })
 
-function getRelativeTime(timestamp: number) {
-  const daysDifference = Math.round((timestamp - new Date().getTime()) / DAY_MILLISECONDS)
+const getMsDifferenceFromToday = (timestamp) => {
+  const todayTimestamp = new Date().getTime()
+  const msDifference = timestamp - todayTimestamp
+  return msDifference
+}
 
-  return rtf.format(daysDifference, FORMATS.day)
+export function getRelativeTime(timestamp: number) {
+  const msDifference = getMsDifferenceFromToday(timestamp)
+
+  console.log(msDifference)
+  // const daysDifference = Math.round(msDifference / DAY_MILLISECONDS)
+  const positiveDifference = msDifference * -1
+
+  if (positiveDifference >= MONTH_MILLISECONDS) {
+  } else if (positiveDifference >= DAY_MILLISECONDS) {
+    console.log('Day')
+  } else if (positiveDifference >= HOUR_MILLISECONDS) {
+    console.log('Hour')
+  } else if (positiveDifference >= MIN_MILLISECONDS) {
+    console.log('Minute')
+    const minDifference = Math.round(msDifference / MIN_MILLISECONDS)
+    return rtf.format(minDifference, 'minute')
+  }
+  // const relative = rtf.format(msDifference / MIN_MILLISECONDS, 'minute')
+  // console.log(relative)
+  // return rtf.format(daysDifference, FORMATS.day)
 }
