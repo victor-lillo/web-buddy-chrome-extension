f9f157e6735ebe9b019bdd1384221a98e1f9200e
fentosv <fentos.dev@gmail.com>
2023-07-27 17:53:28 +0200

    Add Form component and implement

---
+++ src/components/Form.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  function validate() {
+    console.log("I'm the validate() function")
+  }
+</script>
+
+<form on:submit|preventDefault={validate}>
+  <label for="story">Add the URLs you want to block</label>
+  <textarea id="story" name="story" rows="5" cols="33" />
+  <button type="submit"> Save me! </button>
+</form>
+
+<style>
+  form {
+    display: flex;
+    flex-direction: column;
+    font-size: 1rem;
+    gap: 0.5rem;
+  }
+
+  textarea {
+    resize: vertical;
+    max-height: 200px;
+  }
+
+  button {
+    width: fit-content;
+  }
+</style>
--- src/pages/Popup.svelte
+++ src/pages/Popup.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
   import Time from '../components/Time.svelte'
+  import Form from '../components/Form.svelte'
 
   export let relativeTime: string
 </script>
 
 <section>
   <Time {relativeTime} />
+  <Form />
 </section>
 
 <style>
   section {
     display: flex;
     flex-direction: column;
+    gap: 1rem;
   }
 </style>
