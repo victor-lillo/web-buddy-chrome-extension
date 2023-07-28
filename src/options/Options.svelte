3774e22b2b51276f76dea1a8f139cf6205a23858
fentosv <fentos.dev@gmail.com>
2023-07-28 17:51:06 +0200

    Add Options page component

---
+++ src/options/Options.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import HandleUrls from '../components/HandleUrls.svelte'
+  import Form from '../components/Form.svelte'
+
+  export let blockedUrls
+
+  const handleClick = () => {
+    chrome.runtime.openOptionsPage()
+  }
+</script>
+
+<section>
+  <Form />
+  <HandleUrls {blockedUrls} />
+</section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    min-height: 80%;
+  }
+</style>
