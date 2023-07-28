ea832d0bf8de12429c4e3ad7f6c135480c335bdb
fentosv <fentos.dev@gmail.com>
2023-07-28 18:28:51 +0200

    Add mockups urls

--- src/options/index.ts
+++ src/options/index.ts
@@ -4,7 +4,7 @@ import Options from './Options.svelte'
 const target = document.getElementById('full-page-app')
 
 async function render() {
-  const blockedUrls = ['url1', 'url2', 'url2']
+  const blockedUrls = ['url1', 'url2', 'url3', 'url4']
   new Options({ target, props: { blockedUrls } })
 }
 
