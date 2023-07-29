7532fb453e03c85be0954058c3cb70254695820a
fentosv <fentos.dev@gmail.com>
2023-07-29 12:12:54 +0200

    Add ClearStorage dev component

---
+++ src/components/dev/ClearStorage.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { clearStorage } from '../../utils/storage'
+
+  const handleClick = async () => {
+    const storage = await clearStorage()
+    console.log(storage)
+  }
+</script>
+
+{#if import.meta.env.DEV}
+  <button on:click={handleClick}>Clear Storage</button>
+{/if}
+
+<style>
+  button {
+    font-size: 1.1rem;
+    color: var(--color-light-1);
+    background-color: #9328cc;
+    border: 1px solid transparent;
+    border-radius: var(--border-radius);
+    padding: 0.6rem 1.2rem;
+    width: fit-content;
+  }
+
+  button:is(:hover, :focus):not(:disabled) {
+    background-color: #9e3ed1;
+    box-shadow: var(--box-shadow-hover);
+  }
+
+  button:disabled {
+    opacity: 0.6;
+  }
+</style>
