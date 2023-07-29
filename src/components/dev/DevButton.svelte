b14d641c8c052b06365eae04e40ad322fa2cc8af
fentosv <fentos.dev@gmail.com>
2023-07-29 14:08:10 +0200

    Add variants to DevButton & style tweaks

--- src/components/dev/DevButton.svelte
+++ src/components/dev/DevButton.svelte
@@ -1,29 +1,38 @@
 <script lang="ts">
+  type Variants = 'red'
   export let text: string
   export let handleClick: svelte.JSX.MouseEventHandler<HTMLButtonElement>
+  export let variant: Variants | undefined = undefined
 </script>
 
 {#if import.meta.env.DEV}
-  <button on:click={handleClick}>{text}</button>
+  <button on:click={handleClick} class={variant}>{text}</button>
 {/if}
 
 <style>
   button {
-    font-size: 1.1rem;
+    --background-color: #009e20;
+    --border-color: crimson;
+    font-size: 1rem;
     color: var(--color-light-1);
-    background-color: #9328cc;
-    border: 1px solid transparent;
+    background-color: var(--background-color);
+    border: 4px solid var(--border-color);
     border-radius: var(--border-radius);
-    padding: 0.6rem 1.2rem;
+    padding: 0.4rem 1rem;
     width: fit-content;
   }
 
-  button:is(:hover, :focus):not(:disabled) {
-    background-color: #9e3ed1;
+  button:is(:hover):not(:disabled) {
+    filter: brightness(0.9);
     box-shadow: var(--box-shadow-hover);
   }
 
   button:disabled {
     opacity: 0.6;
+  }
+
+  .red {
+    --background-color: crimson;
+    --border-color: #009e20;
   }
 </style>
