2d8fd6eafc31c41de24d51484e5264978fd49f70
fentosv <fentos.dev@gmail.com>
2023-07-29 12:15:56 +0200

    Implement ClearStorage dev component

--- src/components/HandleUrls.svelte
+++ src/components/HandleUrls.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { setStorage } from '../utils/storage'
   import GetWholeStorage from './dev/GetWholeStorage.svelte'
+  import ClearStorage from './dev/ClearStorage.svelte'
 
   export let defaultBlockedUrls: [string]
   let isSelectAll = false
@@ -55,6 +56,7 @@
       {userBlockedUrls.toString()}
     {/if}
   </fieldset>
+  <ClearStorage />
   <GetWholeStorage />
   <button on:click={handleSave} disabled={userBlockedUrls.length === 0}>Delete</button>
 </section>
