9d97e726f8e4a80e8a149ad9f636321d2a69fd4a
fentosv <fentos.dev@gmail.com>
2023-07-29 11:55:50 +0200

    Add components for debugging while dev

---
+++ src/components/dev/GetFullStorage.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { getWholeStorage } from '../../utils/storage'
+
+  const getStorage = async () => {
+    const storage = await getWholeStorage()
+    console.log(storage)
+  }
+</script>
+
+{#if import.meta.env.DEV}
+  <button on:click={getStorage}>Get Storage</button>
+{/if}
+
+<style>
+  button {
+    font-size: 1.1rem;
+    color: var(--color-light-1);
+    background-color: #9328cc;
+    border: 1px solid transparent;
+    border-radius: var(--border-radius);
+    padding: 0.6rem 1.2rem;
+    width: fit-content;
+  }
+
+  button:is(:hover, :focus):not(:disabled) {
+    background-color: #9e3ed1;
+    box-shadow: var(--box-shadow-hover);
+  }
+
+  button:disabled {
+    opacity: 0.6;
+  }
+</style>
