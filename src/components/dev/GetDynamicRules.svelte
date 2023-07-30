77dfdf99aa6186a727f678a7e18d5893dfeb5185
fentosv <fentos.dev@gmail.com>
2023-07-30 11:56:48 +0200

    Add dev component GetDynamicRules

---
+++ src/components/dev/GetDynamicRules.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Button from '../Button.svelte'
+
+  const text = 'Log Rules'
+
+  const handleClick = async () => {
+    const rules = await chrome.declarativeNetRequest.getDynamicRules()
+    rules.forEach((rule) => {
+      console.log(rule)
+    })
+  }
+</script>
+
+<Button {handleClick} {text} variant={'dev-green'} />
