be1cdb52378b4b79bd4367909218686fefd46bc5
fentosv <fentos.dev@gmail.com>
2023-07-30 11:15:55 +0200

    Split setRules method into a new file /rules

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,46 +1,16 @@
+import { DEFAULT_BLOCKED_URLS } from '../DEFAULTS'
+import { setRules } from '../utils/rules'
 import onInstall from './onInstall'
 
-const REDIRECT = chrome.declarativeNetRequest.RuleActionType.REDIRECT
-const MAIN_FRAME = chrome.declarativeNetRequest.ResourceType.MAIN_FRAME
-const SUB_FRAME = chrome.declarativeNetRequest.ResourceType.SUB_FRAME
-const XMLHTTPREQUEST = chrome.declarativeNetRequest.ResourceType.XMLHTTPREQUEST
-
-const redirectExtensionPath = '/src/blocked/blocked.html'
-
 console.log('Service worker started.')
-
-const blockUrls = ['||example', '||example1', '||examples']
 
 chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((e) => {
   const msg = `Navigation blocked to ${e.request.url} on tab ${e.request.tabId}.`
   console.log(msg)
 })
 
-blockUrls.forEach((domain, index) => {
-  let id = index + 1
-
-  chrome.declarativeNetRequest.updateDynamicRules({
-    addRules: [
-      {
-        id: id,
-        priority: 1,
-        action: {
-          type: REDIRECT,
-          redirect: {
-            extensionPath: redirectExtensionPath,
-          },
-        },
-        condition: {
-          urlFilter: domain,
-          resourceTypes: [MAIN_FRAME, SUB_FRAME, XMLHTTPREQUEST],
-        },
-      },
-    ],
-    removeRuleIds: [id],
-  })
-})
-
 onInstall()
+setRules(DEFAULT_BLOCKED_URLS)
 
 // chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
 //   const keywords = ['example']
---
+++ src/utils/rules.ts
@@ -0,0 +1,32 @@
+const REDIRECT = chrome.declarativeNetRequest.RuleActionType.REDIRECT
+const MAIN_FRAME = chrome.declarativeNetRequest.ResourceType.MAIN_FRAME
+const SUB_FRAME = chrome.declarativeNetRequest.ResourceType.SUB_FRAME
+const XMLHTTPREQUEST = chrome.declarativeNetRequest.ResourceType.XMLHTTPREQUEST
+
+const REDIRECT_BLOCKED_PAGE = '/src/blocked/blocked.html'
+
+export async function setRules(urls: Array<string>) {
+  urls.forEach((domain: string, index: number) => {
+    let id = index + 1
+
+    chrome.declarativeNetRequest.updateDynamicRules({
+      addRules: [
+        {
+          id: id,
+          priority: 1,
+          action: {
+            type: REDIRECT,
+            redirect: {
+              extensionPath: REDIRECT_BLOCKED_PAGE,
+            },
+          },
+          condition: {
+            urlFilter: `||${domain}`,
+            resourceTypes: [MAIN_FRAME, SUB_FRAME, XMLHTTPREQUEST],
+          },
+        },
+      ],
+      removeRuleIds: [id],
+    })
+  })
+}
