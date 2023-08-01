569f1ab7eeead66a217e0c732aaf2ff5bc9a2aec
fentosv <fentos.dev@gmail.com>
2023-08-01 11:46:05 +0200

    Add styles in Blocked page

--- src/blocked/Blocked.svelte
+++ src/blocked/Blocked.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
   export let relativeTime: string
+  export let originUrl: string
 </script>
 
 <section>
   <h1>Don't give up, you're doing great!</h1>
+
+  <p>Web Buddy has prevented the following page from loading:</p>
+  <code>
+    {originUrl}
+  </code>
   <p>
-    You have been blocking garbage since <span>{relativeTime}</span>
+    You have been blocking garbage since <strong>{relativeTime}</strong>, stay focused!
   </p>
 </section>
 
@@ -26,7 +32,14 @@
     font-size: 1.5rem;
   }
 
-  span {
+  code {
+    background-color: var(--color-dark-13);
+    font-size: 1.2rem;
+    padding: 0.5rem 1rem;
+    border-radius: var(--border-radius);
+  }
+
+  strong {
     font-weight: var(--font-weight-bold);
   }
 </style>
