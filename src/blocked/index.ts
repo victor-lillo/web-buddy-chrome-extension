dd04d40a1ac481645b03e24fc5d158173e30b5c7
fentosv <fentos.dev@gmail.com>
2023-08-01 11:40:09 +0200

    Add originUrl from url params in blocked/index

--- src/blocked/index.ts
+++ src/blocked/index.ts
@@ -1,6 +1,6 @@
 import '../styles/app.css'
 import Blocked from './Blocked.svelte'
-import { STORAGE_KEYS } from './../DEFAULTS'
+import { STORAGE_KEYS, URL_PARAM_ORIGIN } from './../DEFAULTS'
 import { getStorage } from '../utils/storage'
 import { getRelativeTime } from '../utils/relativeDate'
 
@@ -9,7 +9,11 @@ const target = document.getElementById('full-page-app')
 async function render() {
   const installDate = await getStorage(STORAGE_KEYS.installDate)
   const relativeTime = getRelativeTime(installDate)
-  new Blocked({ target, props: { relativeTime } })
+
+  const params = new URLSearchParams(document.location.search)
+  const originUrl = params.get(URL_PARAM_ORIGIN)
+
+  new Blocked({ target, props: { relativeTime, originUrl } })
 }
 
 document.addEventListener('DOMContentLoaded', render)
