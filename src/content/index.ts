1b151b940c7491a09503012377bfa9129d4c6699
fentosv <fentos.dev@gmail.com>
2023-08-01 09:09:06 +0200

    Comment content.ts

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,10 +1,8 @@
-import { storage } from '../utils/storage'
-
 // Some global styles on the page
 // import './styles.css'
 
 // Some JS on the page
-storage.get().then(console.log)
+// storage.get().then(console.log)
 
 // Some svelte component on the page
 // new Counter({ target: document.body })
