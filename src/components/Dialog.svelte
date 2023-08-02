d941d07cf2fea3b14e798233466751bd26c71499
fentosv <fentos.dev@gmail.com>
2023-08-02 17:53:02 +0200

    Add Dialog component

---
+++ src/components/Dialog.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let dialog
+</script>
+
+<dialog bind:this={dialog} on:close>
+  <slot />
+</dialog>
+
+<style>
+  dialog {
+    border: none;
+    border-radius: var(--border-radius);
+  }
+  dialog::backdrop {
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+</style>
