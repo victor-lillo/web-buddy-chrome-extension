4f3c608c617d5e190fa89368805bb238494d7104
fentosv <fentos.dev@gmail.com>
2023-08-02 08:32:45 +0200

    Add nsfw sites as defaults

--- src/DEFAULTS.ts
+++ src/DEFAULTS.ts
@@ -1,3 +1,4 @@
+import { NSFW_SITES } from '../sites/nsfw'
 export const STORAGE_KEYS = {
   installDate: 'installDate',
   blockedUrls: 'blockedUrls',
@@ -5,4 +6,4 @@ export const STORAGE_KEYS = {
 
 export const URL_PARAM_ORIGIN = 'origin'
 
-export const DEFAULT_BLOCKED_URLS = ['marca.com', 'example.com', 'example1.com', 'examples.com']
+export const DEFAULT_BLOCKED_URLS = NSFW_SITES
