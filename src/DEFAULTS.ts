796130cfd43a019342e517d67a4dd897b1e85fc7
fentosv <fentos.dev@gmail.com>
2023-08-02 16:36:44 +0200

    Add default instructive placeholder

--- src/DEFAULTS.ts
+++ src/DEFAULTS.ts
@@ -4,6 +4,11 @@ export const STORAGE_KEYS = {
   blockedUrls: 'blockedUrls',
 }
 
+export const TEXTAREA_PLACEHOLDER = `Insert here the domains (with extension) you want to block.
+Separate them by comma, space or line break.
+  ✅ url1.com, domain2.io
+  ❌ url1, "domain2.io"
+`
 export const URL_PARAM_ORIGIN = 'origin'
 
 export const DEFAULT_BLOCKED_URLS = NSFW_SITES
