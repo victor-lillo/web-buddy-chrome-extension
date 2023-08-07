36d06bcc7e6bdaa61b9ac6e9b1e4585973f66322
fentosv <fentos.dev@gmail.com>
2023-08-07 09:08:27 +0200

    Remove unnecessary content script

--- src/content/index.ts
+++
@@ -1,8 +0,0 @@
-// Some global styles on the page
-// import './styles.css'
-
-// Some JS on the page
-// storage.get().then(console.log)
-
-// Some svelte component on the page
-// new Counter({ target: document.body })
