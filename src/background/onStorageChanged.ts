c0b47a639121ddc4b9f0eb54a374297ddfa6f6a9
fentosv <fentos.dev@gmail.com>
2023-08-07 09:20:43 +0200

    Add optional chaining sort in onStorage

--- src/background/onStorageChanged.ts
+++ src/background/onStorageChanged.ts
@@ -5,7 +5,7 @@ export default async function onStorageChanged() {
   chrome.storage.onChanged.addListener(async (changes, namespace) => {
     for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
       if (key !== STORAGE_KEYS.blockedDomains) return
-      const sortedNewRules = newValue.sort()
+      const sortedNewRules = newValue?.sort()
       await setBlockRules(sortedNewRules)
     }
   })
