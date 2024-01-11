8ee8d6e919d7ab5966bd3418de41f83637c12551
fentosv <fentos.dev@gmail.com>
2024-01-11 19:17:36 +0100

    Format code

--- src/utils/hideLetters.ts
+++ src/utils/hideLetters.ts
@@ -1,4 +1,5 @@
 const DOT_CHAR = '•'
+
 export function hideCharacters(domain: string) {
   const firstChar = domain.charAt(0)
   const hiddenDomain = firstChar + domain.slice(1).replace(/[a-zA-Z0-9]/g, DOT_CHAR)
--- src/utils/processDomainsForm.ts
+++ src/utils/processDomainsForm.ts
@@ -1,5 +1,4 @@
 import { DOMAIN_REGEX } from './../DEFAULTS'
-// Validates domains with subdomains, without protocol
 
 function splitWordsByDelimiter(str: string) {
   const wordsArray = str.split(/[,\s\n]/).map((palabra) => palabra.trim())
@@ -9,6 +8,7 @@ function splitWordsByDelimiter(str: string) {
 function createFeedbackMessage(numberOfNewDomains: number) {
   return numberOfNewDomains > 0 ? `✅ Successfuly added ${numberOfNewDomains} domain(s)` : `😔 No domains were added`
 }
+
 export function processDomainsTextarea({ text, previousDomains }: { text: string; previousDomains: Array<string> }) {
   const userDomains = splitWordsByDelimiter(text)
   const updatedDomains = [...new Set([...previousDomains, ...userDomains])].sort()
